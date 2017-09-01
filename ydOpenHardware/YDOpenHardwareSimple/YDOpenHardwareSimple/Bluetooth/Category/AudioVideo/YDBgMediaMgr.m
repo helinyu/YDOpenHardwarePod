dd3dd5181973e84e2047fa762145d138c8dcdfbe
felix <2319979647@qq.com>
2017-09-01 14:38:34 +0800

    fix change

--- ydOpenHardware/YDOpenHardwareSimple/YDOpenHardwareSimple/Bluetooth/Category/AudioVideo/YDBgMediaMgr.m
+++ ydOpenHardware/YDOpenHardwareSimple/YDOpenHardwareSimple/Bluetooth/Category/AudioVideo/YDBgMediaMgr.m
@@ -12,6 +12,15 @@
 #import "YDMedia.h"
 #import "YYWebImage.h"
 
+#ifndef dispatch_main_async_safe
+#define dispatch_main_async_safe(block)\
+if (strcmp(dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL), dispatch_queue_get_label(dispatch_get_main_queue())) == 0) {\
+block();\
+} else {\
+dispatch_async(dispatch_get_main_queue(), block);\
+}
+#endif
+
 @interface YDBgMediaMgr ()
 
 @property (nonatomic, strong) YDMedia *media;
@@ -30,16 +39,13 @@
     return singleton;
 }
 
-
 - (void)createRemoteCommandCenter {
     MPRemoteCommandCenter *cmdCenter = [MPRemoteCommandCenter sharedCommandCenter];
     
     __weak typeof (self) wSelf = self;
     [cmdCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
         NSLog(@"播放");
-        NSTimeInterval nowSecondTime = [[NSDate date] timeIntervalSince1970];
-        NSTimeInterval lastTime = _media.currentTime;
-        _media.currentTime = (nowSecondTime - _nowSecondTime) + lastTime;
+        _nowSecondTime = [[NSDate date] timeIntervalSince1970];
         [wSelf setLockPlayerWithInfo:_media];
         return MPRemoteCommandHandlerStatusSuccess;
     }];
@@ -48,7 +54,7 @@
         NSLog(@"暂停播放");
         NSTimeInterval nowSecondTime = [[NSDate date] timeIntervalSince1970];
         NSTimeInterval lastTime = _media.currentTime;
-        _media.currentTime = (nowSecondTime - nowSecondTime) + lastTime;
+        _media.currentTime = (nowSecondTime - _nowSecondTime) + lastTime;
         [wSelf setLockPlayerWithInfo:_media];
         return MPRemoteCommandHandlerStatusSuccess;
     }];
@@ -75,18 +81,30 @@
     NSLog(@"设置后台播放信息");
     
     AVAudioSession *session = [AVAudioSession sharedInstance];
+    AudioSessionSetActive(YES);
     [session setActive:YES error:nil];
     [session setCategory:AVAudioSessionCategoryPlayback error:nil];
     
     NSMutableDictionary * songDict = @{}.mutableCopy;
-    [songDict setObject:@"test" forKey:MPMediaItemPropertyTitle];
-    [songDict setObject:@"test" forKey:MPMediaItemPropertyArtist];
-    UIImage *image = [UIImage imageNamed:@"backgroundImage5.jpg"];
-    [songDict setObject:[[MPMediaItemArtwork alloc] initWithImage:image]
-                 forKey:MPMediaItemPropertyArtwork];
-    [songDict setObject:@(MPMediaTypeAnyAudio) forKey:MPMediaItemPropertyMediaType];
-    [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:songDict];
-
+    [songDict setObject:media.title forKey:MPMediaItemPropertyTitle];
+    [songDict setObject:media.artist forKey:MPMediaItemPropertyArtist];
+    [songDict setObject:@(media.currentTime) forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
+    [songDict setObject:@(media.totalTime) forKey:MPMediaItemPropertyPlaybackDuration];
+    NSLog(@"thread :%@",[NSThread currentThread]);
+    
+    [[YYWebImageManager sharedManager] requestImageWithURL:[NSURL URLWithString:media.imageUrlString] options:YYWebImageOptionShowNetworkActivity progress:nil transform:nil completion:^(UIImage * _Nullable image, NSURL * _Nonnull url, YYWebImageFromType from, YYWebImageStage stage, NSError * _Nullable error) {
+        if (image) {
+            NSLog(@" block thread :%@",[NSThread currentThread]);
+            dispatch_main_async_safe(^{
+                NSLog(@"safe block thread :%@",[NSThread currentThread]);
+                MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithImage:image];
+                [songDict setObject:artwork forKey:MPMediaItemPropertyArtwork];
+                [songDict setObject:@(MPMediaTypeAnyAudio) forKey:MPMediaItemPropertyMediaType];
+                [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:songDict];
+            });
+        }
+    }];
+ 
 }
 
 @end
